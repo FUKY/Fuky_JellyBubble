8da03bd6c4f2417f87318593cb47b4440e49c947
Vũ Khanh <khanhltv.fuky@gmail.com>
2015-05-17 04:16:54 +0700

    Cho Cao file SaveScore.cs.

---
+++ Technical/Fuky_JellyBubble/Assets/Scripts/SaveScore.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+
+public class SaveScore : MonoBehaviour {
+
+    public Text score;
+    public Text highScoreText;
+    int highScore = 0;
+
+    void Start()
+    {
+
+    }
+
+    void SaveScore()
+    {
+        if (int.Parse(score.text) > highScore)
+        {
+            highScore = int.Parse(score.text);
+            PlayerPrefs.SetInt("score", highScore);
+            PlayerPrefs.Save();
+        }
+    }
+
+    void LoadScore()
+    {
+        highScoreText.text = PlayerPrefs.GetInt("score").ToString();
+    }
+}
